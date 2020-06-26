5b6e2d73e4213b89cde1ffce5641d6aaaf89cb55
Gabriel Figueira <gabrielfigueira98@gmail.com>
2020-06-26 17:41:41 -0400

    first commit

---
+++ server/src/routes.ts
@@ -0,0 +1,23 @@
+import express, { request, response } from 'express';
+import path from 'path';
+import knex from './database/connection';
+
+import PointsController from './controllers/PointsController';
+import ItemsController from './controllers/ItemsController';
+
+const routes = express.Router();
+const pointsController = new PointsController();
+const itemsController = new ItemsController();
+
+
+routes.get('/items', itemsController.index);
+
+routes.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
+
+routes.post('/points', pointsController.create);
+
+routes.get('/points', pointsController.index);
+
+routes.get('/points/:id', pointsController.show);
+
+export default routes;
